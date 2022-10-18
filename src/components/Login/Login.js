f907b84e91a0112671ca6e257e5b1accb85f18ea
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-10-18 15:53:19 +0600

    done all

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 import './Login.css';
 
 const Login = () => {
+    const {signIn} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
+    const handelLogIn = (event) =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        signIn(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            form.reset();
+            navigate(from, {replace: true});
+        })
+        .catch(error => console.error(error))
+    }
     return (
-        <div>
-            <h2>This is login page</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form onSubmit={handelLogIn}>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p>New to ema-john? <Link to='/signup'>Create a new account</Link></p>
         </div>
     );
 };
